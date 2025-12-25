b35de4b91ddf1fb249cf7404e5060079d72f5410
Mahdur <mpchalasani@gmail.com>
2025-12-26 10:20:47 +1100

    averages

---
+++ Operators.py
@@ -0,0 +1,38 @@
+a = 2
+b = 4
+
+print(a**b)
+
+a = 7
+b = 3
+
+print( a/b)
+print(a//b)
+
+# % gives remainder
+print(7%4)
+
+
+#conditional operators
+y = 6
+x = 5
+
+print(y == x)
+print( y != x)
+print((y!=x) == (x!=y))
+print((y == x) == (x == y))
+
+x = 10
+y = 5
+
+print(x > y)
+print(y >x)
+print((x > y) == (y < x))
+print((x < y) != (x > y))
+
+x = 15
+y = 15
+
+print(x>=y)
+print(y<=x)
+
---
+++ average.py
@@ -0,0 +1,10 @@
+maths=98
+science=94
+english=41
+history=95
+geography=11
+
+total = maths+science+english+history+geography
+print("the total of all the 5 subjects is:", total)
+average = total/5
+print("the avergae of all the subjects is:", average)
