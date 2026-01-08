171a09c92cbb4b80bfcea02dd2597ea9cda193ae
Mahdur <mpchalasani@gmail.com>
2026-01-09 10:11:55 +1100

    in and not in

---
+++ membership.py
@@ -0,0 +1,17 @@
+a=5
+b=[1,2,3,4,5]
+
+if a in b:
+    print("yes")
+else:
+    print("no")
+
+text1="hobart"
+text2="perth darwin brisbane adelaid sydney"
+
+if text1 in text2:
+    print("Sydney is in text 2")
+elif text1 not in text2:
+    print("text one is not in text 2")
+else:
+    print("sydney is not in text 2")
