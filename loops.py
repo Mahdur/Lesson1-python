fad4cc1c5125e5cf4fe4a8c514f292ab2f089093
Mahdur <mpchalasani@gmail.com>
2026-01-12 11:42:47 +1100

    loops

--- loops.py
+++ loops.py
@@ -3,3 +3,14 @@ while attempts < 5:
     print("you can still login")
     attempts=attempts+1
     print(f"{5-attempts} attempts left")
+
+word1="meditation"
+for character in word1:
+    print(character,end="-")
+
+word2="encyclopedia"
+print(word2 [::-1])
+print(len(word2))
+for w in word2[::-1]:
+    print(w,end="-")
+
