393a73125394b934810fa9151fbf7df87596bc50
Mahdur <mpchalasani@gmail.com>
2026-01-28 20:42:03 +1100

    patterns

--- patterns.py
+++ patterns.py
@@ -6,7 +6,13 @@
 *****
 """
 
+#for i in range(1, 6):
+    #for l in range(1, i+1):
+        #print("*", end="")
+    #print() 
+number=1
 for i in range(1, 6):
     for l in range(1, i+1):
-        print("*", end="")
+        print(number, end=" ")
+        number=number+1
     print()
