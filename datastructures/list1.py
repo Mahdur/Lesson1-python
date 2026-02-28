a99eddb886412906d653a72537ba8ca4879453dd
Mahdur <mpchalasani@gmail.com>
2026-02-28 14:11:09 +1100

    lists

---
+++ datastructures/list1.py
@@ -0,0 +1,7 @@
+listofwords = ['abc', 'cfc','xyz','aba','1221']
+for word in listofwords:
+    if len(word) > 2:
+        if word [0] == word [-1]:
+            print(word)
+
+        
---
+++ datastructures/list3.py
@@ -0,0 +1,11 @@
+L = [4, 5, 1, 2, 9, 7, 10, 8]
+print("Original List :", L)
+
+#variable to store the sum of
+#the list
+count = 0
+
+#Finding the sum
+for i in L:
+    count+= i
+print(count)
